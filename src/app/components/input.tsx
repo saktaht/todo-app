3d57a0b5d7e005523685d8fa4e01ad90cf69a449
oyu <12345yuu6789@gmail.com>
2024-07-13 23:47:57 +0900

    削除機能の追加

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -2,11 +2,29 @@
 
 import React from 'react'
 import { AddTaskProps } from '../types/add'
+import { FilterType } from '../types/add'
 import {v4 as uuidv4} from 'uuid'
+import { Button } from './button'
 
 export const Input = (): React.JSX.Element => {
   const [text, setText] = React.useState<string>('')
   const [todos, setTodos] = React.useState<AddTaskProps[]>([])
+  const [filter, setFilter] = React.useState<FilterType>('all')
+
+  const filteredTodos = todos.filter((todo) => {
+    switch (filter) {
+      case 'all':
+        return !todo.remove;
+      case 'checked':
+        return todo.checked && !todo.remove;
+      case 'unchecked':
+        return !todo.checked && !todo.remove;
+      case 'removed':
+        return todo.remove;
+      default:
+        return todo;
+    }
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
@@ -22,6 +40,8 @@ export const Input = (): React.JSX.Element => {
       // text ステートの値を value プロパティへ
       value: text,
       id: uuidv4(),
+      checked: false,
+      remove: false,
     };
     setTodos((todos) => [newTodo, ...todos]);
     // フォームへの入力をクリアする
@@ -44,8 +64,54 @@ export const Input = (): React.JSX.Element => {
     })
   }
 
+  const handleCheck = (id: string, checked: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          //分割代入でvalueプロパティを追加
+          return {
+            ...todo,
+            checked
+          }
+        }
+        return todo
+      });
+      return newTodos;
+    })
+  }
+
+  const handleRemove = (id: string, remove: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          //分割代入でvalueプロパティを追加
+          return {
+            ...todo,
+            remove
+          };
+        }
+        return todo
+      });
+    return newTodos;
+  });
+  };
+
+  const handleFilter = (filter: FilterType) => {
+    setFilter(filter);
+  }
+
   return (
     <div className=''>
+      {/* e.target.value: string を Filter型にアサーションする */}
+      <select 
+        defaultValue="all" 
+        onChange={(e) => handleFilter(e.target.value as FilterType)}
+      >
+        <option value="all">すべてのタスク</option>
+        <option value="checked">完了したタスク</option>
+        <option value="unchecked">現在のタスク</option>
+        <option value="removed">ごみ箱</option>
+      </select>
       <form 
         autoComplete="on"
         onSubmit={(e) => {
@@ -68,7 +134,7 @@ export const Input = (): React.JSX.Element => {
         />
       </form>
       <ul>
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           if (!todo) {
             return;
           }
@@ -76,11 +142,24 @@ export const Input = (): React.JSX.Element => {
             
             <li key={todo.id} className='border-2 p-4'>
               <input 
+                type="checkbox"
+                disabled={ todo.remove || filter === 'checked' || filter === 'removed' }
+                checked={todo.checked}
+                onChange={(e) => handleCheck(todo.id, !todo.checked)}
+              />
+              <input 
                 type="text"
+                disabled={ todo.checked || todo.remove || filter === 'checked' || filter === 'removed' }
                 value={todo.value}
                 onChange={e => handleEdit(todo.id, e.target.value)}
               />
-              {/* {todo.value} */}
+              <button 
+                className='px-3 py-1 bg-rose-500 hover:bg-rose-600 text-white rounded-md ml-4'
+                onClick={() => handleRemove(todo.id, !todo.remove)}
+              >
+                {todo.remove ? '復元' : '削除'}
+                {/* 削除 */}
+              </button>
             </li>
           );
         })}
--- src/app/types/add.ts
+++ src/app/types/add.ts
@@ -4,4 +4,8 @@ import {v4 as uuidv4} from 'uuid'
 export type AddTaskProps = {
   value: string;
   readonly id: string;
+  checked?: boolean;
+  remove?: boolean;
 };
+
+export type FilterType = 'all' | 'checked' | 'unchecked' | 'removed';
